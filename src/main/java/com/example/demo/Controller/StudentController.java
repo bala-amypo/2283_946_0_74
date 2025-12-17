04de9bdee9690b7ccfbaae9c117fc4e5a9424d19
bala-amypo <bala@amypo.in>
2025-12-17 08:28:23 +0000

    User commit

--- src/main/java/com/example/demo/Controller/StudentController.java
+++ src/main/java/com/example/demo/Controller/StudentController.java
@@ -1,23 +1,29 @@
 package com.example.demo.Controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation .*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.demo.Entity.StudentEntity;
-import com.example.demo.Service. StudentService;
+import com.example.demo.Service.StudentService;
 
 @RestController
 public class StudentController {
+    
+    @Autowired
+    StudentService ser;
+    
+    @PostMapping("/adddata")
+    public StudentEntity createData(@RequestBody StudentEntity stu){
+        return ser.createData(stu);
+    }
 
-@Autowired
-StudentService ser;
-
-@PostMapping("d")
-public Student createData(@RequestBody Student stu){
-return ser.createData(stu);
-}
-@GetMapping("/fetchrecord")
-public List<Student> fetchRecord(){
-return ser.fetchRecord();
-}
+    @GetMapping("/fetche")
+    public List<StudentEntity> fetchrecord(){
+        return ser.fetchrecord();
+    }
 }
